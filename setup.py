57b82db6ac08c13ebc5d07ea17dc95f501969a17
pavanchhatpar <pavanchhatpar@gmail.com>
2020-05-03 03:54:46 -0400

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type='text/markdown',
     url="https://github.com/pavanchhatpar/copynet-tf",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.1.1",
     author='Pavan Chhatpar',
     author_email='pavanchhatpar@gmail.com'
 )
