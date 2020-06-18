d25cd972982201d04486606520c8ab029e28b491
pavanchhatpar <pavanchhatpar@gmail.com>
2020-06-17 21:35:55 -0400

    both docker images expose ports

--- start-env.sh
+++ start-env.sh
@@ -6,17 +6,17 @@ fi
 
 source "lock.env"
 
-if [ "$1" == "nb" ]; then
-    docker run -it --gpus all --rm\
-        -u $(id -u):$(id -g)\
-        -v "`pwd`:/tf/src"\
-        -v "`echo $DATA_DIR`:`echo $DATA_DIR`"\
-        -p 8991:8888\
-        copynet-tf/$1:latest
-else
-    docker run -it --gpus all --rm\
-        -u $(id -u):$(id -g)\
-        -v "`pwd`:/tf/src"\
-        -v "`echo $DATA_DIR`:`echo $DATA_DIR`"\
-        copynet-tf/$1:latest
-fi
+# if [ "$1" == "nb" ]; then
+docker run -it --gpus all --rm\
+    -u $(id -u):$(id -g)\
+    -v "`pwd`:/tf/src"\
+    -v "`echo $DATA_DIR`:`echo $DATA_DIR`"\
+    -p 8991:8888\
+    copynet-tf/$1:latest
+# else
+#     docker run -it --gpus all --rm\
+#         -u $(id -u):$(id -g)\
+#         -v "`pwd`:/tf/src"\
+#         -v "`echo $DATA_DIR`:`echo $DATA_DIR`"\
+#         copynet-tf/$1:latest
+# fi
